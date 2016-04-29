f30710619f39b4f22f1ff522cff4da17ba271607
hero.dot <kape.wolf@web.de>
2016-04-29 14:14:42 +0200

    help needed on how apply is used

--- Task3.R
+++ Task3.R
@@ -1,23 +1,35 @@
-
-# TODO
-# a)
-# Describe the structure of the response object. Explain dimensions and nesting of the elements
- 
-# b) 
-# Use apply to extract author, title, publishing date, and rating of each book in the response
- 
-# c)
-# Sort the list by date and title
-# getBookList(numerOfItems)
-
-# d) 
-# Function returns value, where book is available and what's its price.
-# Change the API call
-# getBookSalesInfo(response)
-
-
 install.packages("RCurl")
 library(RCurl)
 library(RJSONIO)
 URL = "https://www.googleapis.com/books/v1/volumes?q=george+r+r+martin&maxResults=40"
 response_parsed <- fromJSON(getURL(URL, ssl.verifyhost = 0L, ssl.verifypeer = 0L))
+
+# TODO
+# a)
+# Describe the structure of the response object. Explain dimensions and nesting of the elements
+summary(response_parsed)
+str(response_parsed) 
+
+summary(response_parsed$items)
+str(response_parsed$items[[1]])
+summary(response_parsed$items[[1]])
+summary(response_parsed$items[[1]]$volumeInfo)
+response_parsed$items[[1]]$volumeInfo[c("title","authors","publishedDate","ratingsCount")]
+
+
+# b) 
+# Use apply to extract author, title, publishing date, and rating of each book in the response
+# for every entry in response_parsed$items get the data above
+response_parsed$items[[1]]$volumeInfo
+response_parsed$items[[1]]$volumeInfo$title
+response_parsed$items[[1]]$volumeInfo$authors
+apply(response_parsed$items[],2, function(x) x$volumeInfo)
+ 
+# c)
+# Sort the list by date and title
+# getBookList(numberOfItems)
+
+# d) 
+# Function returns value, where book is available and what's its price.
+# Change the API call
+# getBookSalesInfo(response)
