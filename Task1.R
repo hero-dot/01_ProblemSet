35862e82bdaa64b40ca07aa8d5e0bbe4672148ed
hero.dot <kape.wolf@web.de>
2016-04-29 14:13:41 +0200

    quantile function added

--- Task1.R
+++ Task1.R
@@ -30,18 +30,21 @@ createFormattedAd(vehicleData)
 # Ist das Fahrzeug in den top 10% füge einen entsprechenden Vermerk hinzu.
 
 # Die Top Ten Werte für Leistung, Fuel Efficiency, 1/4 mile time 
-topHp <- quantile(vehiclesData["hp"],probs = 0.9,na.rm = T)
-topMpg <- quantile(vehiclesData["mpg"],probs = 0.9,na.rm = T)
-topQsec <- quantile(vehiclesData["qsec"],probs = 0.1,na.rm = T)
-quantiles <- c(topHp,topMpg, topQsec)
+quantiles <- function(vehiclesData)
+{
+  topHp <- quantile(vehiclesData["hp"],probs = 0.9,na.rm = T)
+  topMpg <- quantile(vehiclesData["mpg"],probs = 0.9,na.rm = T)
+  topQsec <- quantile(vehiclesData["qsec"],probs = 0.1,na.rm = T)
+  return(c(topHp,topMpg,topQsec))
+}
 
 createFormattedAdWithComparisons <- function(vehicleData)
   {vehicleDim <- c("Fuel Efficiency:","Cylinder:","Horse Power:","1/4 mile time:") 
   carname <- row.names(vehicleData)
   
-  if (vehicleData[,1]>= quantiles[2]) tempMpg <- "Top10%" else tempMpg <- ""
-  if (vehicleData[,4]>= quantiles[1]) tempHp <- "Top10%" else tempHp <- ""
-  if (vehicleData[,7]<= quantiles[3]) tempQsec <- "Top10%" else tempQsec <- ""
+  if (vehicleData[,1] >= quantiles(vehiclesData)[2]) tempMpg <- "Top10%" else tempMpg <- ""
+  if (vehicleData[,4] >= quantiles(vehiclesData)[1]) tempHp <- "Top10%" else tempHp <- ""
+  if (vehicleData[,7] <= quantiles(vehiclesData)[3]) tempQsec <- "Top10%" else tempQsec <- ""
   
   evalTop <- c(tempMpg,"",tempHp,tempQsec)
   
