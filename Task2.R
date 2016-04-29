6f223736432b8cc5fadac9bf5a156b4f43b163c1
hero.dot <kape.wolf@web.de>
2016-04-29 13:18:34 +0200

    update Task 2

--- Task2.R
+++ Task2.R
@@ -4,7 +4,7 @@
 # provide an executive summary of the data set
 # Infrastructure for reading in the files
 assemblies <- read.csv("assemblies.csv",header = T)
-billOfMaterials <- read.csv("bill_of_materials.csv", header = T)
+bom <- read.csv("bill_of_materials.csv", header = T)
 components <- read.csv("components.csv", header = T)
 endForm <- read.csv("end_form.csv",header = T)
 quotes <- read.csv("quotes.csv", header = T)
@@ -17,9 +17,9 @@ head(assemblies)
 summary(assemblies)
 
 # Summary billOfMaterials
-str(billOfMaterials)
-head(billOfMaterials)
-summary(billOfMaterials)
+str(bom)
+head(bom)
+summary(bom)
 
 # Summary components
 str(components)
@@ -44,20 +44,32 @@ summary(steps)
 
 # b) 
 # combine the assembly data set with the bill of materials and the steps information
-assemBillofmaterials <- merge(assemblies,billOfMaterials, by = "tube_assembly_id")
+assBoM <- merge(assemblies,billOfMaterials, by = "tube_assembly_id")
 
 # c)
 # change data set from wide to long with the id variable tube_assembly_id
 # How can we assess an assembly's complexity with respect to its specs from this data
+require(reshape2)
+assBoMlong <- melt(assBoM, id.vars = "tube_assembly_id")
+str(assBoMlong)
+summary(assBoMlong)
+# High complexity, because of number of observations and only a few variables.
 
 # d)
 # back to wide format
 # drop variable and cast to wide frame. Do not use the data from bcd
-# What are the dimensions of the object?
-# Which columns are redundant?
-# Do you see a problem with the component columns concerning analysis of pricing patterns?
+assBoMwide <- dcast(assBoMlong,tube_assembly_id ~ variable)
 
+# What are the dimensions of the object?
+dim(assBoMwide)
+
+# Which columns are redundant?
+str(assBoMwide)
+# Do you see a problem with the component columns concerning analysis of pricing patterns?
+unique(assBoMwide$quantity_1)
 
 # f)
-# 
+bomW1 <- dcast(bom, tube_assembly_id~component_id_1,value.var = "quantity_1",fill=0)
+
+bomW8 <- dcast(bom, tube_assembly_id~component_id_8,value.var = "quantity_1",fill=0)
 
